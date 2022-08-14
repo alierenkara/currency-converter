4a90ab8ef0f2c9c4f3a143c3bac331f2df90c727
Ali Erenkara <alierenkara@gmail.com>
2022-08-15 00:05:55 +0300

    prod

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path")
 const Dotenv = require("dotenv-webpack")
 
 module.exports = {
-  mode: "development",
+  mode: "production",
   entry: "./index.js",
   output: {
     path: path.resolve(__dirname, "public"),
